0eb14847115b908ba35a497e52e0faf7b2cb94fb
Peiyan <lip@student.ethz.ch>
2016-04-10 21:17:33 +0200

    update the touchpad control

--- .private_scripts/run_after_start_up.sh
+++ .private_scripts/run_after_start_up.sh
@@ -5,3 +5,10 @@ nitrogen --restore
 synclient HorizTwoFingerScroll=1
 synclient VertScrollDelta=-243
 synclient HorizScrollDelta=-243
+# synclient RTCornerButton=1
+# synclient RBCornerButton=1
+# synclient LTCornerButton=1
+# synclient LBCornerButton=1
+# synclient TapButton1=0
+synclient PalmDetect=1
+synclient VertEdgeScroll=0
